2da6eafa07076eb06435eb124c0674c3de9ee24d
Zhangluo <643733581@qq.com>
2025-03-11 13:48:16 +0800

    :ambulance: perf(): 增加了成功颁发标识后返回标识id

--- controller/didController/create_identity.go
+++ controller/didController/create_identity.go
@@ -9,19 +9,19 @@ import (
 )
 
 // CreateSoftwareIdentity Submit a transaction synchronously, blocking until it has been committed to the ledger.
-func CreateSoftwareIdentity(contract *client.Contract, softwareIdentity model.SoftwareIdentity) error {
+func CreateSoftwareIdentity(contract *client.Contract, softwareIdentity model.SoftwareIdentity) (string, error) {
 	didDocJson, jsonErr := json.Marshal(softwareIdentity)
 	if jsonErr != nil {
 		log.Printf("序列化失败：%v", jsonErr)
-		return fmt.Errorf("序列化失败：%v", jsonErr)
+		return "", fmt.Errorf("序列化失败：%v", jsonErr)
 	}
 	fmt.Printf("\n--> Submit Transaction: CreateSoftwareDID\n")
 	params := []string{softwareIdentity.DID, string(didDocJson)}
 	_, err := contract.SubmitTransaction("CreateSoftwareDID", params...)
 	if err != nil {
 		log.Printf("failed to submit transaction: %v", err)
-		return fmt.Errorf("failed to submit transaction: %v", err)
+		return "", fmt.Errorf("failed to submit transaction: %v", err)
 	}
 	fmt.Printf("*** Transaction committed successfully\n")
-	return nil
+	return softwareIdentity.DID, nil
 }
